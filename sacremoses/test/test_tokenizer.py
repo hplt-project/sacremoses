eed94633880056b22cfde096385b91481723bd4e
alvations <alvations@gmail.com>
2018-04-20 12:34:40 +0800

    corrected typo in variable names

--- sacremoses/test/test_tokenizer.py
+++ sacremoses/test/test_tokenizer.py
@@ -29,8 +29,8 @@ class TestTokenzier(unittest.TestCase):
         text = "This ain't funny. It's actually hillarious, yet double Ls. | [] < > [ ] & You're gonna shake it off? Don't?"
         expected_tokens_with_xmlescape = ['This', 'ain', '&apos;t', 'funny', '.', 'It', '&apos;s', 'actually', 'hillarious', ',', 'yet', 'double', 'Ls', '.', '&#124;', '&#91;', '&#93;', '&lt;', '&gt;', '&#91;', '&#93;', '&amp;', 'You', '&apos;re', 'gonna', 'shake', 'it', 'off', '?', 'Don', '&apos;t', '?']
         expected_tokens_wo_xmlescape = ['This', 'ain', "'t", 'funny', '.', 'It', "'s", 'actually', 'hillarious', ',', 'yet', 'double', 'Ls', '.', '|', '[', ']', '<', '>', '[', ']', '&', 'You', "'re", 'gonna', 'shake', 'it', 'off', '?', 'Don', "'t", '?']
-        assert moses.tokenize(sent, escape=True) == expected_tokens_with_xmlescape
-        assert moses.tokenize(sent, escape=False) == expected_tokens_wo_xmlescape
+        assert moses.tokenize(text, escape=True) == expected_tokens_with_xmlescape
+        assert moses.tokenize(text, escape=False) == expected_tokens_wo_xmlescape
 
 
 class TestDetokenizer(unittest.TestCase):
@@ -43,8 +43,8 @@ class TestDetokenizer(unittest.TestCase):
         expected_detokens = = u'This , is a sentence with weird \xbb symbols \u2026 appearing everywhere \xbf'
         assert md.detokenize(expected_tokens) == expected_detokens
 
-        sent = "This ain't funny. It's actually hillarious, yet double Ls. | [] < > [ ] & You're gonna shake it off? Don't?"
+        text = "This ain't funny. It's actually hillarious, yet double Ls. | [] < > [ ] & You're gonna shake it off? Don't?"
         expected_tokens = [u'This', u'ain', u'&apos;t', u'funny', u'.', u'It', u'&apos;s', u'actually', u'hillarious', u',', u'yet', u'double', u'Ls', u'.', u'&#124;', u'&#91;', u'&#93;', u'&lt;', u'&gt;', u'&#91;', u'&#93;', u'&amp;', u'You', u'&apos;re', u'gonna', u'shake', u'it', u'off', u'?', u'Don', u'&apos;t', u'?']
         expected_detokens = "This ain't funny. It's actually hillarious, yet double Ls. | [] < > [] & You're gonna shake it off? Don't?"
-        assert mt.tokenize(sent) == expected_tokens
+        assert mt.tokenize(text) == expected_tokens
         assert md.detokenize(expected_tokens) == expected_detokens
