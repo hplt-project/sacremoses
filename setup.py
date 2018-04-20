ce1159beb49f734f8c4902a2c005437f498b20e7
alvations <alvations@gmail.com>
2018-04-20 13:35:06 +0800

    added six dependencies

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ from distutils.core import setup
 setup(
   name = 'sacremoses',
   packages = ['sacremoses'],
-  version = '0.0.0',
+  version = '0.0.1',
   description = 'SacreMoses',
+  long_description = 'LGPL MosesTokenizer in Python'
   author = '',
   license = '',
   package_data={'sacremoses': ['data/perluniprops/*.txt', 'data/nonbreaking_prefixes/nonbreaking_prefix.*']},
   url = 'https://github.com/alvations/sacremoses',
   keywords = [],
   classifiers = [],
+  install_requires = ['six']
 )
