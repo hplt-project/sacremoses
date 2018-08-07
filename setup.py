787de1dfdfc51d6423c075c2841a708cdd932b1c
alvations <alvations@gmail.com>
2018-08-07 15:08:16 +0800

    up version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'sacremoses',
   packages = ['sacremoses'],
-  version = '0.0.3',
+  version = '0.0.4',
   description = 'SacreMoses',
   long_description = 'LGPL MosesTokenizer in Python',
   author = '',
