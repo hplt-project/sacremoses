457a5c4c2ad746f4529519cef88e38a0d420e694
David Harrison <david@xnor.com>
2019-04-20 18:41:10 -0700

    Remove unrelated test.

--- sacremoses/test/test_tokenizer.py
+++ sacremoses/test/test_tokenizer.py
@@ -61,12 +61,6 @@ class TestTokenzier(unittest.TestCase):
         expected_tokens = "The meeting will take place at 11 : 00 a.m. Tuesday .".split()
         self.assertEqual(moses.tokenize(text), expected_tokens)
 
-    def test_period_apostrophe(self):
-        moses = MosesTokenizer()
-        text = "'Hello.'"
-        expected_tokens = "&apos;Hello . &apos;".split()
-        self.assertEqual(moses.tokenize(text), expected_tokens)
-
     def test_protect_patterns(self):
         moses = MosesTokenizer()
         text = "this is a webpage https://stackoverflow.com/questions/6181381/how-to-print-variables-in-perl that kicks ass"
