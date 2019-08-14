813773ace1ac811ba49b74cd837c116879d956d9
alvations <alvations@gmail.com>
2019-08-14 17:44:30 +0800

    be more specific with the variables

--- sacremoses/__init__.py
+++ sacremoses/__init__.py
@@ -5,4 +5,4 @@ from sacremoses.normalize import *
 
 # from sacremoses.subwords import *
 
-__version__ = "0.0.32"
+__version__ = "0.0.33"
--- sacremoses/tokenize.py
+++ sacremoses/tokenize.py
@@ -283,7 +283,7 @@ class MosesTokenizer(object):
         BASIC_PROTECTED_PATTERN_5,
     ]
 
-    def __init__(self, lang="en", custom_nonbreaking_prefixes=None):
+    def __init__(self, lang="en", custom_nonbreaking_prefixes_file=None):
         # Initialize the object.
         super(MosesTokenizer, self).__init__()
         self.lang = lang
@@ -294,7 +294,7 @@ class MosesTokenizer(object):
         ]
 
         # Load custom nonbreaking prefixes file.
-        if custom_nonbreaking_prefixes:
+        if custom_nonbreaking_prefixes_file:
             self.NONBREAKING_PREFIXES  = []
             with open(custom_nonbreaking_prefixes, 'r') as fin:
                 for line in fin:
