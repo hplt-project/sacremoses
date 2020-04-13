4e7e648daa0939cbf4e243872fd478e74708ac44
alvations <alvations@gmail.com>
2020-04-13 11:32:04 +0800

    up version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ sacremoses=sacremoses.cli:cli
 setup(
   name = 'sacremoses',
   packages = ['sacremoses'],
-  version = '0.0.39',
+  version = '0.0.40',
   description = 'SacreMoses',
   long_description = 'LGPL MosesTokenizer in Python',
   author = '',
