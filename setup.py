90376dfaf0f41399a090e7620feb3c2494f865a6
alvations <alvations@gmail.com>
2020-05-04 10:26:40 +0800

    upgraded version

--- setup.py
+++ setup.py
@@ -9,15 +9,18 @@ sacremoses=sacremoses.cli:cli
 setup(
   name = 'sacremoses',
   packages = ['sacremoses'],
-  version = '0.0.41',
+  version = '0.0.43',
   description = 'SacreMoses',
-  long_description = 'LGPL MosesTokenizer in Python',
+  long_description = 'MosesTokenizer in Python',
   author = '',
-  license = '',
   package_data={'sacremoses': ['data/perluniprops/*.txt', 'data/nonbreaking_prefixes/nonbreaking_prefix.*']},
   url = 'https://github.com/alvations/sacremoses',
   keywords = [],
-  classifiers = [],
+  classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
   install_requires = ['regex', 'six', 'click', 'joblib', 'tqdm'],
   entry_points=console_scripts,
 )
