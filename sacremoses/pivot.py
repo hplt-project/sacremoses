dc417a5cfce152f1489f8f18db5fc87c38a1bea4
alvations <alvations@gmail.com>
2021-05-02 09:20:01 -0400

    improved pivot

--- sacremoses/pivot.py
+++ sacremoses/pivot.py
@@ -11,22 +11,31 @@ def xxh64(s, seed=42, digest='int'):
     else:
         return xxhash.xxh64(s, seed=seed)
 
-def pivot_bitext(src, pivot1, pivot2, trg):
+def pivot_bitext(src, pivot1, pivot2, trg, outputfile='pivoted.tsv'):
     # Run the hash on the files.
     with open(pivot1) as fin:
         pivot1_hashes = [xxh64(line) for line in tqdm(fin)]
+    with open(pivot2) as fin:
         pivot2_hashes = [xxh64(line) for line in tqdm(fin)]
     # Find the overlaps.
     overlaps = set(pivot1_hashes).intersection(pivot2_hashes)
-    # Dictionary to keep the pivoted result.
-    data = defaultdict(lambda: defaultdict(set))
+    # Keep tracks on one src_pivot1.
+    hash2src = {}
+    hash2pivot1 = {}
     # Iterate through the first lang pair, populate `data`.
-    with open(src) as sfin, open(pivot1) as tfin:
+    with open(src) as sfin, open(pivot1) as tfin, open('src_pivot1.tsv', 'w') as fout:
+        print('\t'.join(['hash', 'src', 'pivot1']), end='\n', file=fout)
         for s, t, hash in tqdm(zip(sfin, tfin, pivot1_hashes)):
             if hash in overlaps:
-                data[t]['p1'].add(s)
+                hash2src[hash] = s.strip()
+                hash2pivot1[hash] = t.strip()
+                print('\t'.join([hash, s.strip(), t.strip()]), end='\n', file=fout)
     # Iterate through the second lang pair, populate `data`.
-    with open(pivot1) as sfin, open(trg) as tfin:
+    with open(pivot2) as sfin, open(trg) as tfin, \
+    open('pivot2_trg.tsv', 'w') as fout, open(outputfile, 'w') as fout_pivot:
+        print('\t'.join(['hash', 'pivot2', 'trg']), end='\n', file=fout)
+        print('\t'.join(['hash', 'src', 'pivot', 'trg']), end='\n', file=fout)
         for s, t, hash in tqdm(zip(sfin, tfin, pivot2_hashes)):
-            if hash in overlaps:
-                data[s]['p2'].add(s)
+            if hash in overlaps and hash2src[hash] == pivot2:
+                print('\t'.join([hash, s.strip(), t.strip()]), end='\n', file=fout)
+                print('\t'.join([hash, hash2pivot1[hash], s.strip(), t.strip()]), end='\n', file=fout_pivot)
