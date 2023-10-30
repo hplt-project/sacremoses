4ffe8ed0b9b2cbd79aad8902a5546b8f442f52fc
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2023-10-30 15:53:42 +0000

    Fill in version and long description from files
    Messed up the 0.1.0 release. Next time we'll do better.

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import re
+import os
 from setuptools import setup
 
 console_scripts = """
@@ -5,12 +7,21 @@ console_scripts = """
 sacremoses=sacremoses.cli:cli
 """
 
+with open(os.path.join(os.path.dirname(__file__), 'sacremoses/__init__.py'), 'r') as fh:
+  match = re.search(r'''^__version__\s*=\s*(["'])(.+?)\1\s*$''', fh.read(), flags=re.MULTILINE)
+  assert match, "count not find __version__ in sacremoses/__init__.py"
+  version = match.group(2)
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as fh:
+  long_description = fh.read()
+
 setup(
   name = 'sacremoses',
   packages = ['sacremoses'],
-  version = '0.1.0',
+  version = version,
   description = 'SacreMoses',
-  long_description = 'MosesTokenizer in Python',
+  long_description = long_description,
+  long_description_content_type = 'text/markdown',
   author = '',
   package_data={'sacremoses': ['data/perluniprops/*.txt', 'data/nonbreaking_prefixes/nonbreaking_prefix.*']},
   url = 'https://github.com/hplt-project/sacremoses',
